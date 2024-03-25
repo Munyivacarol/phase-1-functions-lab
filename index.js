01d5f43c486c32604c42fe8dc6099850bbdf611b
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-03-25 17:07:38 +0300

    done functions lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+// Code your solution in this file//
+
+ function distanceFromHqInBlocks(destination){
+ const block = 42;
+ if(block > destination){
+     return block - destination;
+ }
+ else if (block < destination){
+     return destination - block;
+}
+ }
+//calculating distance from hq in feet//
+function distanceFromHqInFeet(destination){
+         distanceFromHqInBlocks(destination);
+         return distanceFromHqInBlocks(destination) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+if(start > destination){
+    return (start-destination) * 264;
+}
+else{
+    return (destination-start) * 264;
+}
+}
+//calculating
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if(distance <= 400){
+        return  0;
+    }
+    else if(distance > 400 && distance<= 2000){
+        return .02 *(distance - 400);
+    }
+    else if(distance > 2000 && distance < 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
